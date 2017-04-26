750e7def151c1a4851cb5916d97b8366ff2e6779
NsemaX <nsemax@hotmail.com>
2017-04-26 15:13:35 +0300

    initial commit

---
+++ MultiLanguageWebSite/Controllers/LanguageController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultiLanguageWebSite.Controllers
+{
+    public class LanguageController : Controller
+    {
+        public ActionResult Change(string lang)
+        {
+            if (lang != null)
+            {
+
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(lang);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(lang);
+
+            }
+            HttpCookie cookie = new HttpCookie("Language");
+            cookie.Value = lang;
+            Response.Cookies.Add(cookie);
+
+            return View("Change");
+        }
+
+    }
+}
